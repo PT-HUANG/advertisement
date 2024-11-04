93a960afb5308a31c99e5f8c98343f04cc4bd4c3
PT-HUANG <qwe1593574268@gmail.com>
2024-11-04 17:08:31 +0800

    build: JS interactive functions

--- src/main.js
+++ src/main.js
@@ -1,2 +1,113 @@
 import './scss/main.scss'
-console.log('JS loaded!')
+
+const frame = document.querySelector('.frame')
+const folder = document.querySelector('.folder')
+const mail = document.querySelector('.mail')
+const optionItems = document.querySelectorAll('.options > div')
+const listContainer = document.querySelector('.list-container')
+
+
+// 點擊sidebar顯示效果
+optionItems.forEach(element => {
+    element.addEventListener('click', () => {
+        for (let i=0; i< optionItems.length; i++) {
+            optionItems[i].classList.remove('bg-black')
+            optionItems[i].classList.remove('bg-white')
+        }
+        if(element.classList.contains('frame')) {
+            element.classList.add('bg-white')
+        }
+        else if (element.classList.contains('folder') || element.classList.contains('mail')) {
+            element.classList.add('bg-black')
+        }
+    })
+});
+
+// 動態產生內容
+function generateInnerHtml (num) {
+    for(let i=1; i<= num; i++) {
+        listContainer.innerHTML += `
+            <div class="list">
+                <div class="checkbox-wraper">
+                    <input type="checkbox" class="checkbox">
+                </div>
+                <div class="id">
+                    <p>1</p>
+                </div>
+                <div class="name">
+                    <p>Christmas 2020</p>
+                </div>
+                <div class="advertiser">
+                    <p>Food Market</p>
+                    <p>Krab</p>
+                </div>
+                <div class="description">
+                    <p>holiday night during chr...</p>
+                </div>
+                <div class="price">
+                    <p>2000</p>
+                </div>
+                <div class="start-time">
+                    <p>2020/12/24 19:30</p>
+                </div>
+                <div class="end-time">
+                    <p>2021/01/02 23:00</p>
+                </div>
+                <div class="actions">
+                    <img src="icons/menu.svg" alt="">
+                    <div class="popup d-none">
+                        <div class="popup-items">
+                            <img src="icons/duplicate.svg" alt="">
+                            <p>Duplicate</p>
+                        </div>
+                        <div class="popup-items">
+                            <img src="icons/edit.svg" alt="">
+                            <p>Edit</p>
+                        </div>
+                        <div class="popup-items">
+                            <img src="icons/delete.svg" alt="">
+                            <p>Delete</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+}
+
+generateInnerHtml(16)
+
+// 偶數表格灰底顏色
+const listItems = document.querySelectorAll('.list')
+for(let i=0; i<listItems.length; i++) {
+    if(i%2 !== 0) {
+        listItems[i].classList.add('odd')
+    } else {
+        listItems[i].classList.add('even')
+    }
+}
+
+// 勾選變橘色
+const checkboxes = document.querySelectorAll('.list .checkbox')
+checkboxes.forEach(element => {
+    element.addEventListener('click', () => {
+        const list = element.parentElement.parentElement
+        if (list.classList.contains('odd')) {
+            list.classList.remove('odd')
+            list.classList.toggle('orange')
+        } else if (list.classList.contains('even')) {
+            list.classList.toggle('orange')
+        } else {
+            list.classList.add('odd')
+        }
+    })
+})
+
+//POP UP toggle
+const button = document.querySelectorAll('.actions > img')
+const popup = document.querySelectorAll('.popup')
+button.forEach(element => {
+    element.addEventListener('click', () => {
+        element.nextElementSibling.classList.toggle('d-none')
+    })
+})
